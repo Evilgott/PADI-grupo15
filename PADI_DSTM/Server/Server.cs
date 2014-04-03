b8bc258ac077eb6642adcb701d066f522d2ec54d
luis-henriques <luis.c.a.henriques@gmail.com>
2014-04-03 17:54:31 +0100

    Inicio de definição de estrutura do projecto

---
+++ PADI_DSTM/Server/Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using PADI_Library;
+
+namespace Server
+{
+    class Server : RemoteServer
+    {
+        private TcpChannel _channel;
+        private int _port;
+
+        Server(int port)
+        {
+            _port = port;
+            _channel = new TcpChannel(_port);
+            ChannelServices.RegisterChannel(_channel, true);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(RemoteServer),
+                "MyRemoteObjectName",
+                WellKnownObjectMode.Singleton);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.Write("Insert the server port: ");
+            int port = Convert.ToInt32(System.Console.ReadLine());
+            Server server = new Server(port);
+            
+
+
+
+            
+
+            System.Console.WriteLine("<enter> para sair...");
+            System.Console.ReadLine();
+        }
+    }
+}
