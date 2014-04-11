8dbdb41b461f1e1a247a8c8cabafa40c0bccd2e2
Evilgott <sebastiaomaya2005@hotmail.com>
2014-04-11 13:00:17 +0100

    added more structure

---
+++ PADI_DSTM/Client/ClientWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using PADI_Library;
+
+namespace Client
+{
+    public partial class ClientWindow : Form
+    {
+        private TcpChannel _channel;
+        private RemoteMasterServer _rMasterServer;
+        private PadInt _copyVal;
+
+        public ClientWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeRemoteMasterServer()
+        {
+            RemotingConfiguration.RegisterWellKnownClientType(
+            typeof(RemoteMasterServer), "tcp : // <ip-address >:< 8086 > /MasterServer"); //port e ip address fixos, MasterServer
+        }
+
+        private void connectToMasterServer()
+        {
+            _channel = new TcpChannel(8086);
+            ChannelServices.RegisterChannel(_channel, true);
+            InitializeRemoteMasterServer();
+
+            _rMasterServer = (RemoteMasterServer)Activator.GetObject(
+                typeof(RemoteMasterServer),
+                "tcp : // <ip-address >:< 8086 > /MasterServer");
+        }
+
+        private void ClientWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PADI_DSTM/PADI_Library/Interfaces.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PADI_Library
+{
+    public class RemoteMasterServer : MarshalByRefObject
+    {
+        private int _tId;
+        private Dictionary<PadInt, Dictionary<RemoteServer, RemoteServer>> _servers;
+    }
+
+    public class RemoteServer : MarshalByRefObject
+    {
+        private enum _state { normal, frozen, failing };
+        private string _url;
+        //private Dictionary<PadInt, TX> _wTX; write transactions
+        //private Dictionary<PadInt, TX> _rTX; read transactions
+        //private List<Requests> _rq;
+    }
+
+    public class Library
+    {
+        //URL = "tcp : // <ip-address >:< port > /Server"
+        //TODO:
+        /*
+        bool Init();
+        bool TxBegin();
+        bool TxCommit();
+        bool TxAbort();
+        bool Status();
+        bool Fail(string URL);
+        bool Freeze(string URL);
+        bool Recover(string URL);
+
+        PadInt CreatePadInt(int uid);
+        PadInt AcessPadInt(int uid);
+         * */
+    }
+}
